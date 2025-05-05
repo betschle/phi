de7203c15aec61225701abf45fedcfb7cac01bef
betty <b5cully@yahoo.de>
2025-05-05 13:35:32 +0200

    Migration: UsageChecker & tests

---
+++ core/src/main/java/com/neutronio/phi/util/UsageChecker.java
@@ -0,0 +1,58 @@
+package com.neutronio.phi.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tracks usage of string based ids to determine
+ * unused objects in a repository or other data storage.
+ */
+public class UsageChecker {
+    private List<String> allIDs = new ArrayList<>();
+    private List<String> usedIDs = new ArrayList<>();
+
+    public UsageChecker( List<String> keys) {
+        this.allIDs.addAll(keys);
+    }
+
+    public UsageChecker() {
+
+    }
+
+    /**
+     * For modifying the original ID set.
+     * Do not use {@link #record(String)} until all available
+     * IDs have been added
+     * @param id
+     */
+    public void addId( String id) {
+        this.allIDs.add(id);
+    }
+    /**
+     * Records a String and marks it as used
+     * @param id must be contained inside allIDs
+     */
+    public void record( String id) {
+        if( this.allIDs.contains(id) )
+            this.usedIDs.add(id);
+    }
+
+    /**
+     * Gets a collection of unused strings
+     * @return
+     */
+    public List<String> getUnusedStrings() {
+        List<String> unusedIDs = new ArrayList<>();
+        for( String id : this.allIDs) {
+            if( !this.usedIDs.contains(id) ) {
+                unusedIDs.add(id);
+            }
+        }
+        return unusedIDs;
+    }
+
+    public int getTotalStringCount() {
+        return this.allIDs.size();
+    }
+}
+
---
+++ core/src/test/java/com/neutronio/phi/util/UsageCheckerTest.java
@@ -0,0 +1,42 @@
+package com.neutronio.phi.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UsageCheckerTest {
+
+    @Test
+    public void unusedString() {
+        List<String> ids = Arrays.asList("test", "test2", "test3");
+
+        UsageChecker usageChecker = new UsageChecker(ids);
+
+        usageChecker.record("test2");
+        usageChecker.record("test9");
+        usageChecker.record("test");
+
+        List<String> unusedStrings = usageChecker.getUnusedStrings();
+        Assertions.assertTrue( unusedStrings.size() == 1);
+        Assertions.assertTrue( unusedStrings.contains("test3"));
+        Assertions.assertFalse( unusedStrings.contains("test2"));
+    }
+
+    @Test
+    public void nothingRecorded() {
+        List<String> ids = Arrays.asList("test", "test2", "test3");
+
+        UsageChecker usageChecker = new UsageChecker(ids);
+
+        usageChecker.record("Dada");
+        usageChecker.record("lalal");
+        usageChecker.record("lalal");
+
+        List<String> unusedStrings = usageChecker.getUnusedStrings();
+        Assertions.assertTrue( unusedStrings.size() == 3);
+        Assertions.assertTrue( unusedStrings.contains("test3"));
+        Assertions.assertTrue( unusedStrings.contains("test2"));
+    }
+}
