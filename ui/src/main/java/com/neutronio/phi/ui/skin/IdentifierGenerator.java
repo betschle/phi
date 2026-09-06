fb9b3d590e8130a1468936a971f020d91d9530b4
betty <b5cully@yahoo.de>
2026-09-06 08:37:08 +0200

    Added file operation translations via Generator

--- ui/src/main/java/com/neutronio/phi/ui/skin/IdentifierGenerator.java
+++ ui/src/main/java/com/neutronio/phi/ui/skin/IdentifierGenerator.java
@@ -1,6 +1,12 @@
 package com.neutronio.phi.ui.skin;
 
+import com.neutronio.phi.io.FileHandler;
 import com.neutronio.phi.lang.TranslationGenerator;
+import com.neutronio.phi.ui.ComponentFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Generates translation files for the phi-ui
@@ -8,10 +14,19 @@ import com.neutronio.phi.lang.TranslationGenerator;
 public class IdentifierGenerator {
 
     public static void main(String[] args) {
+        TranslationGenerator.TranslationConfiguration config = new TranslationGenerator.TranslationConfiguration();
+        Properties phiDefaults = new Properties();
+        try {
+            phiDefaults.load(new FileInputStream("ui/assets/lang/phi.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        config.properties.add(phiDefaults);
+        config.enumTranslations = ComponentFactory.getDefaultEnumTranslations();
+
         TranslationGenerator translationGenerator = new TranslationGenerator();
-        // doesnt work with resources?
         translationGenerator.generateAndSaveCode(
-            "ui/assets/lang/phi.properties",  "PhiUITranslations",
+            config,  "PhiUITranslations",
             "com.neutronio.phi.ui.skin", "ui/src/main/java/" );
     }
 }
